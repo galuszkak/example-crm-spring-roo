2d6589bc347b3f116ee3806381f260303be87b8d
Kamil Gałuszka <galuszkak@gmail.com>
2013-02-03 10:56:11 +0100

    deleting old files

--- src/main/java/com/solution4future/enums/ActionType.java
+++
@@ -1,6 +0,0 @@
-package com.solution4future.enums;
-
-
-public enum ActionType {
-    START_CONTACT, REMIND, GIVE_DISCOUNT;
-}
--- src/main/java/com/solution4future/enums/Status.java
+++
@@ -1,6 +0,0 @@
-package com.solution4future.enums;
-
-
-public enum Status {
-    NO_CONTACT, CONTACT_AGAIN, NOT_INTRESTED, CLIENT;
-}
